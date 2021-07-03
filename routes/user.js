1b369f5aed99e4e3ac7e8743f8271c28b060b5ed
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-07-04 00:32:32 +0530

    updated bcrypt to bcryptJS

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Notification = require("../Model/Notification");
 const Comment = require("../Model/Comment");
